2438e60f809dd6fe915ce80e14f5b19e904ac401
Mehul Raheja <rahejamehul@gmail.com>
2020-07-05 20:17:01 -0700

    Base files

---
+++ inject.js
@@ -0,0 +1,27 @@
+x = document.getElementsByClassName('yt-simple-endpoint inline-block style-scope ytd-thumbnail');
+
+
+function replaceThumbnail(element,src){
+	element.getElementsByTagName('img')[0].src = src
+}
+
+function replaceTitle(element,title){
+
+}
+
+function removeHover(element){
+
+}
+
+function changeChannel(element){
+
+}
+
+function changeInfo(element){
+
+}
+
+replaceThumbnail(x[0],'https://i.ytimg.com/vi/RqQGUJK7Na4/hq720.jpg');
+//TODO:
+// Implement Change functions
+// 
